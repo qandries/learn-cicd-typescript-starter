e14ac0bede811e96ce676b268d52ebc18c306cf4
qandries <andries.quentin@gmail.com>
2025-01-23 15:13:22 +0100

    add unused

--- src/main.ts
+++ src/main.ts
@@ -44,6 +44,10 @@ v1Router.get("/healthz", handlerReadiness);
 
 app.use("/v1", v1Router);
 
+function unused() {
+  
+}
+
 app.listen(config.api.port, () => {
   console.log(`Server is running on port: ${config.api.port}`);
 });
