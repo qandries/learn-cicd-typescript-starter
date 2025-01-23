096ea545e9e975a4ce4c547c9aa35b6bf3123c39
qandries <andries.quentin@gmail.com>
2025-01-23 15:27:13 +0100

    tsc

--- eslint.config.js
+++ eslint.config.js
@@ -6,8 +6,9 @@ import pluginSecurity from "eslint-plugin-security";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { ignores: ["dist/*"] },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginSecurity.configs.recommended,
