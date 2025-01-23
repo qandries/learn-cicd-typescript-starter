119628ea7ef52c8f00696c355c7cc4cc9e7e11cf
qandries <andries.quentin@gmail.com>
2025-01-23 15:19:48 +0100

    add security

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
+import pluginSecurity from "eslint-plugin-security";
 
 
 /** @type {import('eslint').Linter.Config[]} */
@@ -9,4 +10,5 @@ export default [
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  pluginSecurity.configs.recommended,
 ];
